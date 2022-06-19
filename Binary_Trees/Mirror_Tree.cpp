86bf0c53de397b452f0232d6a2a4f5f7dd3d5e3f
Venkata Sai Tanish Jonnadula <87538078+venkatasaitanish@users.noreply.github.com>
2022-06-19 13:37:07 +0530

    Create Mirror_Tree.cpp

---
+++ Binary_Trees/Mirror_Tree.cpp
@@ -0,0 +1,11 @@
+class Solution {
+  public:
+    void mirror(Node* node) {
+        if(node==NULL) return;
+        swap(node->left,node->right);
+        mirror(node->left);
+        mirror(node->right);
+    }
+};
+
+// https://practice.geeksforgeeks.org/problems/mirror-tree/1#
